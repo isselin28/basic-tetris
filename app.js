e01eff7dd91e4777b086d0c11f87c8977a36519a
isselin28 <isselin.moektijono@gojek.com>
2022-04-15 15:59:50 +0800

    draw board and tetromino

---
+++ app.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  let squares = Array.from(document.querySelectorAll(".grid div"));
+  const ScoreDisplay = document.querySelector("#score");
+  const StartBtn = document.querySelector("#start-button");
+
+  const width = 10;
+
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2],
+  ];
+
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+  ];
+
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1],
+  ];
+
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+  ];
+
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+  ];
+
+  const theTetrominoes = [lTetromino, zTetromino, oTetromino, iTetromino];
+
+  let currentPosition = 3;
+  let currentRotation = 0;
+  //randomly select tetromino
+  let random = Math.floor(Math.random() * theTetrominoes.length);
+  let current = theTetrominoes[0][0];
+
+  //draw the first roation in the first tetromino
+
+  function draw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.add("tetromino");
+    });
+  }
+
+  //undraw the tetromino
+  function undraw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.remove("tetromino");
+    });
+  }
+});
