c0fa9ff5a1946b044345425112a0f99e5dd61e07
isselin28 <isselin.moektijono@gojek.com>
2022-04-16 15:24:17 +0800

    rotate tetromino

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (e.keyCode === 37) {
       moveLeft();
     } else if (e.keyCode === 38) {
-      //move up
+      rotate();
     } else if (e.keyCode === 39) {
       moveRight();
     } else if (e.keyCode === 40) {
@@ -140,6 +140,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (isTaken) currentPosition -= 1;
 
+    draw();
+  }
+
+  //rotate the tetromino
+  function rotate() {
+    undraw();
+    currentRotation++;
+    if (currentRotation === current.length) {
+      // if the current rotation gets to 4, make it go back to 0
+      currentRotation = 0;
+    }
+    current = theTetrominoes[random][currentRotation];
     draw();
   }
 });
